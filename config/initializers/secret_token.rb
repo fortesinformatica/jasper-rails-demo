10ebb7a390c6ef13b91d44c496f2bef60c7116ce
Rodrigo Maia <rodrigomb@gmail..com>
2012-05-17 14:49:45 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasperRailsDemo::Application.config.secret_token = '0c0b9cbce7aeb7bcf2ee24954fc4283a0aa3706cc54bd70360c52e90bf629b934537bec28a1b6b8046169861884db5932189fb83f2ee8a267df53382e51c58c1'
