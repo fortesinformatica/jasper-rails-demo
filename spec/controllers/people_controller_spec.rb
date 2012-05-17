a3f55044f8bf7c3f3ef98a61efdaad7537ffd2d2
Rodrigo Maia <rodrigomb@gmail..com>
2012-05-17 17:50:10 -0300

    Add a rspec test

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -3,21 +3,22 @@ require 'spec_helper'
 describe PeopleController do
 	describe "GET listagem" do
     before do
-      Person.stub(:all).and_return([Person.new(:name=>'Teste', :email=>'tet@tet.com'), Person.new(:name=>'Teste2', :email=>'tet2@tet.com')])
+      Person.stub(:all).and_return([Person.new(:name=>'jonh', :email=>'lennon@beatles.com'), Person.new(:name=>'paul', :email=>'paul@beatles.com')])
     end
     
     it "be success" do
       response.should be_success
     end
     
-    it "dont have nulls" do
+    it "should not contain nulls" do
       get :index, :format => :pdf
       response.should_not contain("null")
     end
 
-    it "contains email" do
+    it "should contain emails" do
       get :index, :format => :pdf
-      response.should contain("tet@tet.com")
+      response.should contain("lennon@beatles.com")
+      response.should contain("paul@beatles.com")
     end
 	end
 end
